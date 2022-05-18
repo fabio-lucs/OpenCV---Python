5750f51cd173f9e27271dbf02c06a850f68f3559
fabio-lucs <fabiolucs19@gmail.com>
2022-05-18 14:28:07 -0300

    up
    Atualizando..

--- src/main.py
+++ src/main.py
@@ -2,3 +2,16 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
+def show_Image(img):
+    obj_img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    plt.imshow(obj_img)
+    plt.show()
+
+
+
+def main():
+    img = cv2.imread('src/imgs/ballons.jpeg')
+    show_Image(img)
+
+main()    
+
